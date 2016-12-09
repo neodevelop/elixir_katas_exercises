ce9d835bda7ce63c739da1280fadc5b1a14df9fd
neodevelop <juan@makingdevs.com>
2016-12-08 22:29:10 -0600

    String Calculator Refactor, thanks to @Sohjiro...

--- lib/string_calculator/string_calculator.ex
+++ lib/string_calculator/string_calculator.ex
@@ -1,27 +1,21 @@
 defmodule StringCalculator do
   import String, only: :functions
 
-  def add(s) when byte_size(s) == 0 , do: 0
+  def add(""), do: 0
   def add(s) do
     s
-    |> evaluate_if_contains_custom_separator
+    |> handle_the_separator
     |> replace("\n",",")
     |> split(",")
     |> Enum.map(&to_integer(&1))
     |> Enum.reduce(0, fn(x, acc) -> x + acc end)
   end
 
-  defp evaluate_if_contains_custom_separator(s) do
-    cond do
-      starts_with?(s, "//") == true -> change_separator_type(s)
-      true -> s
-    end
+  defp handle_the_separator(<< "//", separator :: binary-size(1), "\n", rest :: binary >>) do
+    rest
+    |> replace(separator, ",")
   end
 
-  defp change_separator_type(s) do
-    s
-    |> slice(4..-1)
-    |> replace(at(s, 2), ",")
-  end
+  defp handle_the_separator(s), do: s
 
 end
