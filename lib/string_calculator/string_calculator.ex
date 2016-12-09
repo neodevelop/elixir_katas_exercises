11aeb9d1e3a94f95bef9000302b0ded8e06d1cae
neodevelop <juan@makingdevs.com>
2016-12-08 22:24:30 -0600

    String Calculator Using the import of String

--- lib/string_calculator/string_calculator.ex
+++ lib/string_calculator/string_calculator.ex
@@ -1,26 +1,29 @@
 defmodule StringCalculator do
+  import String, only: :functions
+
   def add(s) when byte_size(s) == 0 , do: 0
-  if String.starts_with? s, "//" do
-    s
-    |> evaluate_separators
-    |> String.replace("\n",",")
-    |> String.split(",")
-    |> Enum.map(&String.to_integer(&1))
-    |> Enum.reduce(0, fn(x, acc) -> x + acc end)
-  else
-    s
-    |> String.replace("\n",",")
-    |> String.split(",")
-    |> Enum.map(&String.to_integer(&1))
-    |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+  def add(s) do
+    if starts_with? s, "//" do
+      s
+      |> evaluate_separators
+      |> replace("\n",",")
+      |> split(",")
+      |> Enum.map(&String.to_integer(&1))
+      |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+    else
+      s
+      |> replace("\n",",")
+      |> split(",")
+      |> Enum.map(&String.to_integer(&1))
+      |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+    end
   end
-end
 
-defp evaluate_separators(s) do
-  separator = String.at s, 2
-  s
-  |> String.slice(4..-1)
-  |> String.replace(separator, ",")
+  defp evaluate_separators(s) do
+    separator = at s, 2
+    s
+    |> slice(4..-1)
+    |> replace(separator, ",")
+  end
 
-end
 end
