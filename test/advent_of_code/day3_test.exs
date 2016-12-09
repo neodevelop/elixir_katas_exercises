b4be313103ff19e91bfcd941177d94874898cc44
neodevelop <juan@makingdevs.com>
2016-12-08 22:59:19 -0600

    Adjusting modules

--- test/advent_of_code/day3_test.exs
+++ test/advent_of_code/day3_test.exs
@@ -1,6 +1,6 @@
 defmodule AdventOfCodeTest.Day3 do
   use ExUnit.Case
-  doctest AdventOfCode
+  doctest AdventOfCode.Day3
 
   test "> delivers presents to 2 houses" do
     assert AdventOfCode.Day3.deliver_gifts(">") == 2
@@ -26,4 +26,3 @@ defmodule AdventOfCodeTest.Day3 do
     assert AdventOfCode.Day3.deliver_gifts_with_robot("^v^v^v^v^v") == 11
   end
 end
-
--- test/advent_of_code/day4_test.exs
+++ test/advent_of_code/day4_test.exs
@@ -1,15 +1,18 @@
 defmodule AdventOfCodeTest.Day4 do
   use ExUnit.Case
-  doctest AdventOfCode
+  doctest AdventOfCode.Day4
 
+  @tag :skip
   test "If secret key is is 'abcdef', the first try is with 0" do
     assert AdventOfCode.Day4.assemble('abcdef',0) == 'abcdef000000'
   end
 
+  @tag :skip
   test "If your secret key is 'abcdef', the mining is 609043" do
     assert AdventOfCode.Day4.mining("abcdef") == 609043
   end
 
+  @tag :skip
   test "If your secret key is 'pqrstuv', the mining is abcdef" do
     assert AdventOfCode.Day4.mining("abcdef") == 1048970
   end
