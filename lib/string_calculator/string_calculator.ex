72ae2478aaf912ec4c94627a8d88c0a030a1d68f
neodevelop <juan@makingdevs.com>
2016-12-08 22:26:23 -0600

    String Calculator Refactor

--- lib/string_calculator/string_calculator.ex
+++ lib/string_calculator/string_calculator.ex
@@ -3,20 +3,14 @@ defmodule StringCalculator do
 
   def add(s) when byte_size(s) == 0 , do: 0
   def add(s) do
-    if starts_with? s, "//" do
-      s
-      |> evaluate_separators
-      |> replace("\n",",")
-      |> split(",")
-      |> Enum.map(&to_integer(&1))
-      |> Enum.reduce(0, fn(x, acc) -> x + acc end)
-    else
-      s
-      |> replace("\n",",")
-      |> split(",")
-      |> Enum.map(&to_integer(&1))
-      |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+    cond do
+      starts_with?(s, "//") == true -> evaluate_separators(s)
+      true -> s
     end
+    |> replace("\n",",")
+    |> split(",")
+    |> Enum.map(&to_integer(&1))
+    |> Enum.reduce(0, fn(x, acc) -> x + acc end)
   end
 
   defp evaluate_separators(s) do
