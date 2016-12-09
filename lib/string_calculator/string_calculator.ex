d34ad3012c57410b0a90019618c6bfd749e80a72
neodevelop <juan@makingdevs.com>
2016-12-08 22:27:29 -0600

    String Calculator Adding a function to express the solution, not the final refactor

--- lib/string_calculator/string_calculator.ex
+++ lib/string_calculator/string_calculator.ex
@@ -3,17 +3,22 @@ defmodule StringCalculator do
 
   def add(s) when byte_size(s) == 0 , do: 0
   def add(s) do
-    cond do
-      starts_with?(s, "//") == true -> evaluate_separators(s)
-      true -> s
-    end
+    s
+    |> evaluate_if_contains_custom_separator
     |> replace("\n",",")
     |> split(",")
     |> Enum.map(&to_integer(&1))
     |> Enum.reduce(0, fn(x, acc) -> x + acc end)
   end
 
-  defp evaluate_separators(s) do
+  defp evaluate_if_contains_custom_separator(s) do
+    cond do
+      starts_with?(s, "//") == true -> change_separator_type(s)
+      true -> s
+    end
+  end
+
+  defp change_separator_type(s) do
     s
     |> slice(4..-1)
     |> replace(at(s, 2), ",")
