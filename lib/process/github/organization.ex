7cc5c54474f8a14f8f00e05bd6f1d4cf04b5bb28
neodevelop <juan@makingdevs.com>
2016-12-31 14:07:29 -0600

    Parsing response from Github and extracting info from users

--- lib/process/github/organization.ex
+++ lib/process/github/organization.ex
@@ -9,10 +9,29 @@ defmodule GitHub.Organization do
     organization_name
     |> create_url
     |> HTTPoison.get(@headers, @http_options)
+    |> parse_response
+    |> extract_login_information
   end
 
   defp create_url(organization_name) do
     @organization_members_url
     |> String.replace(":org", organization_name)
   end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, headers: _headers, status_code: 200}}) do
+    body
+    |> JSON.decode
+  end
+
+  defp parse_response({_, %HTTPoison.Response{body: body, headers: headers, status_code: _}}) do
+    IO.inspect body
+    IO.inspect headers
+    raise "ops"
+  end
+
+  defp extract_login_information({:ok, users_info}) do
+    users_info
+    |> Enum.map(fn %{"id" => id, "login" => login} -> {id, login} end)
+  end
+
 end
