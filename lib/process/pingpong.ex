bea52af843a73b2761c52d226a48b04c071a402c
neodevelop <juan@makingdevs.com>
2016-12-30 22:18:23 -0600

    Adding some isolated functions, and docs for notes

--- lib/process/pingpong.ex
+++ lib/process/pingpong.ex
@@ -1,5 +1,32 @@
 defmodule PingPong do
 
+  @doc """
+  Make a million of calls to the same message
+
+  ## Examples:
+
+      iex> p = spawn(PingPong, :ping_no_response, [])
+      :ok
+      iex> 1..1_000_000 |> Stream.map(fn _ -> send(p, :ping) end) |> Enum.count
+      :ok
+
+  """
+  def ping_no_response do
+    receive do
+      :ping ->
+        IO.puts "Pong and continue..."
+    end
+    ping_no_response
+  end
+
+  def pong_no_response do
+    receive do
+      :pong ->
+        IO.puts "Ping and continue..."
+    end
+    pong_no_response
+  end
+
   def ping(n) do
     receive do
       {pid, x} ->
