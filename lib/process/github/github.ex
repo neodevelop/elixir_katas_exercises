fdc52c3f30fa4f2e75e6be02cd0344bf74f394f5
neodevelop <juan@makingdevs.com>
2017-01-02 22:09:06 -0600

    Refactor, using import for Enum and Map

--- lib/process/github/github.ex
+++ lib/process/github/github.ex
@@ -1,10 +1,12 @@
 defmodule GitHub do
+  import Enum, only: [map: 2, reduce: 3]
+  import Map, only: [merge: 2]
 
   def users_and_followers_of(organization) do
     GitHub.Organization.members_of(organization)
-    |> Enum.map(&(extract_username(&1)))
-    |> Enum.map(&(GitHub.User.followers_of/1))
-    |> Enum.reduce(%{},(fn({username, followers}, acc) -> Map.merge acc, %{username => followers} end))
+    |> map(&(extract_username(&1)))
+    |> map(&(GitHub.User.followers_of/1))
+    |> reduce(%{},(fn({username, followers}, acc) -> merge acc, %{username => followers} end))
   end
 
   defp extract_username({_id, username}) do
