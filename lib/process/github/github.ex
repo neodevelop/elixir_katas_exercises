b5b9d810eb237194288fbc6d4529e2b7c2d90ac4
neodevelop <juan@makingdevs.com>
2017-01-02 22:04:48 -0600

    Making a map

--- lib/process/github/github.ex
+++ lib/process/github/github.ex
@@ -4,6 +4,7 @@ defmodule GitHub do
     GitHub.Organization.members_of(organization)
     |> Enum.map(&(extract_username(&1)))
     |> Enum.map(&(GitHub.User.followers_of/1))
+    |> Enum.reduce(%{},(fn({username, followers}, acc) -> Map.merge acc, %{username => followers} end))
   end
 
   defp extract_username({_id, username}) do
