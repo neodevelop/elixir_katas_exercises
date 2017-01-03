8513dc3de3d009ca7233161896eff74df06340a5
neodevelop <juan@makingdevs.com>
2017-01-02 18:07:59 -0600

    Obtaining the followers of one user

---
+++ lib/process/github/user.ex
@@ -0,0 +1,41 @@
+defmodule GitHub.User do
+
+  @followers_url "https://api.github.com/users/:username/followers"
+  @http_options [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
+  @access_token Application.get_env(:elixir_katas_exercises, :access_token)
+  @headers ["Authorization": "token #{@access_token}"]
+
+  def followers_of(username) do
+    username
+    |> create_url
+    |> make_a_request
+    |> parse_response
+    |> extract_followers_info
+  end
+
+  defp create_url(username) do
+    @followers_url
+    |> String.replace(":username", username)
+  end
+
+  defp make_a_request(url) do
+    HTTPoison.get(url, @headers, @http_options)
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, headers: _headers, status_code: 200}}) do
+    body
+    |> JSON.decode
+  end
+
+  defp parse_response({_, %HTTPoison.Response{body: body, headers: headers, status_code: _}}) do
+    IO.inspect body
+    IO.inspect headers
+    raise "ops"
+  end
+
+  defp extract_followers_info({:ok, users_info}) do
+    users_info
+    |> Enum.map(fn %{"id" => id, "login" => login} -> {id, login} end)
+  end
+
+end
