258412a74c388b2a023088cc9684fef9bd1fbe8f
neodevelop <juan@makingdevs.com>
2017-02-01 22:34:39 -0600

    Adding a coordinator

--- lib/process/github/coordinator.ex
+++ lib/process/github/coordinator.ex
@@ -1,11 +1,19 @@
 defmodule GitHub.Coordinator do
 
-  def loop() do
+  def loop(results \\ [], results_expected) do
     receive do
-      {:ok, members} when is_list(members) ->
-
+      {:ok, result} ->
+        new_results = [results | result]
+        if Enum.count(new_results) == results_expected do
+          send self, :exit
+        end
+        IO.inspect new_results
+        IO.inspect results_expected
+        loop(new_results, results_expected)
+      :exit ->
+        IO.puts(results |> Enum.sort |> Enum.join(','))
       _ ->
-        send sender_id, "Unknown message"
+        loop(results, results_expected)
     end
   end
 
