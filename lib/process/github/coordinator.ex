01c660a9b1419f9ee659548e56ce036fa29a1ee3
neodevelop <juan@makingdevs.com>
2017-02-04 13:04:02 -0600

    Change in showing info and joining results

--- lib/process/github/coordinator.ex
+++ lib/process/github/coordinator.ex
@@ -3,15 +3,15 @@ defmodule GitHub.Coordinator do
   def loop(results \\ [], results_expected) do
     receive do
       {:ok, result} ->
-        new_results = [results | result]
+        new_results = results ++ [result]
         if Enum.count(new_results) == results_expected do
           send self, :exit
         end
-        IO.inspect new_results
-        IO.inspect results_expected
         loop(new_results, results_expected)
       :exit ->
-        IO.puts(results |> Enum.sort |> Enum.join(','))
+        results
+        |> Enum.sort
+        |> IO.inspect
       _ ->
         loop(results, results_expected)
     end
