189a127b22c832a58ec25a7e5c62981a552bb8c9
neodevelop <juan@makingdevs.com>
2017-02-06 16:01:12 -0600

    Using a GenServer

---
+++ lib/otp/my_genserver.ex
@@ -0,0 +1,19 @@
+defmodule BankAccountGS do
+  use GenServer
+
+  def init(balance) do
+    {:ok, balance}
+  end
+
+  def handle_cast({:deposit, amount}, balance) do
+    {:noreply, balance + amount}
+  end
+
+  def handle_cast({:withdraw, amount}, balance) do
+    {:noreply, balance - amount}
+  end
+
+  def handle_call(:balance, _from, balance) do
+    {:reply, balance, balance}
+  end
+end
