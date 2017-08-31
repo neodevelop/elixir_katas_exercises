6b900a511f2118c265e93d185aa7efb8875b1b0d
neodevelop <juan@makingdevs.com>
2017-08-30 22:51:38 -0500

    Adjusting for better use

--- lib/advent_of_code/day4.ex
+++ lib/advent_of_code/day4.ex
@@ -2,13 +2,14 @@ defmodule AdventOfCode.Day4 do
 
   def assemble(key, n) do
     s = cond do
-      n >= 0 && n < 10 -> '000000' ++ Integer.to_char_list n
-      n >= 10 && n < 100 -> '00000' ++ Integer.to_char_list n
-      n >= 100 && n < 1000 -> '0000' ++ Integer.to_char_list n
-      n >= 1000 && n < 10000 -> '000' ++ Integer.to_char_list n
-      n >= 10000 && n < 100000 -> '00' ++ Integer.to_char_list n
-      n >= 100000 && n < 1000000 -> '0' ++ Integer.to_char_list n
-      n >= 1000000 -> Integer.to_char_list n
+      n >= 0 && n < 10 -> '00000' ++ Integer.to_char_list n
+      n >= 10 && n < 100 -> '0000' ++ Integer.to_char_list n
+      n >= 100 && n < 1000 -> '000' ++ Integer.to_char_list n
+      n >= 1000 && n < 10000 -> '00' ++ Integer.to_char_list n
+      n >= 10000 && n < 100000 -> '0' ++ Integer.to_char_list n
+      true -> Integer.to_char_list n
+      # n >= 100000 && n < 1000000 -> Integer.to_char_list n
+      # n >= 1000000 -> Integer.to_char_list n
     end
     key ++ s
   end
@@ -22,11 +23,14 @@ defmodule AdventOfCode.Day4 do
   end
 
   def searchForSecret(secret, n, _) do
-    l = :crypto.hash(:md5 , assemble(secret,n))
-        |> Base.encode16()
-        |> String.to_char_list
+    l = makeTheHash(secret, n)
     searchForSecret(secret, n+1, Enum.take(l,5))
   end
 
+  def makeTheHash(secret, n) do
+    :crypto.hash(:md5 , assemble(secret,n))
+    |> Base.encode16()
+    |> String.to_char_list
+  end
 
 end
