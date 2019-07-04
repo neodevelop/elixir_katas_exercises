12986662d5f42b7698b606815852e19d6a68dd64
neodevelop <juan@makingdevs.com>
2019-07-03 21:02:01 -0500

    Add a test case

--- test/advent_of_code/day6_test.exs
+++ test/advent_of_code/day6_test.exs
@@ -5,6 +5,7 @@ defmodule AdventOfCodeTest.Day6 do
   [
     {"turn on 0,0 through 999,999", {:turn_on, [0, 0], [999, 999]}},
     {"toggle 0,0 through 999,0", {:toggle, [0, 0], [999, 0]}},
+    {"turn off 499,499 through 500,500", {:turn_off, [499, 499], [500, 500]}},
   ] |> Enum.each(fn {instruction, {expected_action, expected_corner_1, expected_corner_2}} ->
 
     @instruction instruction
