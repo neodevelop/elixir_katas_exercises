9ecf9f3b9c23762d7395edf4e15f47ef4810c4eb
neodevelop <juan@makingdevs.com>
2019-07-03 20:58:57 -0500

    Refactor in test for multiple tests

--- test/advent_of_code/day6_test.exs
+++ test/advent_of_code/day6_test.exs
@@ -2,13 +2,23 @@ defmodule AdventOfCodeTest.Day6 do
   use ExUnit.Case
   doctest AdventOfCode.Day6
 
-  test "make instructions for 'turn on 0,0 through 999,999'" do
-    instruction = "turn on 0,0 through 999,999"
-    {action, corner1, corner2} = AdventOfCode.Day6.parse_instruction(instruction)
-    assert action == :turn_on
-    assert corner1 == [0, 0]
-    assert corner2 == [999, 999]
+  [
+    {"turn on 0,0 through 999,999", {:turn_on, [0, 0], [999, 999]}},
+    {"toggle 0,0 through 999,0", {:toggle, [0, 0], [999, 0]}},
+  ] |> Enum.each(fn {instruction, {expected_action, expected_corner_1, expected_corner_2}} ->
 
-  end
+    @instruction instruction
+    @expected_action expected_action
+    @expected_corner_1 expected_corner_1
+    @expected_corner_2 expected_corner_2
+
+    test "make instructions for '#{@instruction}'" do
+      {action, corner1, corner2} = AdventOfCode.Day6.parse_instruction(@instruction)
+      assert action == @expected_action
+      assert corner1 == @expected_corner_1
+      assert corner2 == @expected_corner_2
+    end
+
+  end)
 
 end
