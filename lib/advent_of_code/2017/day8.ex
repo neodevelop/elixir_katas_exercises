ed1ba3a95d0c0792d098b3bc9805543073eaf54d
neodevelop <juan@makingdevs.com>
2020-03-15 23:49:04 -0600

    Add first test case and implementation for counting

--- lib/advent_of_code/2017/day8.ex
+++ lib/advent_of_code/2017/day8.ex
@@ -1,4 +1,25 @@
-defmodule AdventOfCode.Day6 do
+defmodule AdventOfCode.Day8 do
+
+  def count_literals_and_memory_for(input) do
+    {count_string_literals(input), count_memory_values(input)}
+  end
+
+  def count_string_literals(input) do
+    input
+    |> Enum.count()
+  end
+
+  def count_memory_values(input) do
+    input
+    |> make_the_word("")
+    |> String.length()
+  end
+
+  def make_the_word([?" | []], word), do: word
+  def make_the_word([?" | t], word) when length(t) > 0, do: make_the_word(t, word)
+  def make_the_word([c | t], word) when length(t) > 0 do
+    make_the_word(t, word <> List.to_string([c]))
+  end
 
 
 end
--- test/advent_of_code/day8_test.exs
+++ test/advent_of_code/day8_test.exs
@@ -1,5 +1,21 @@
-defmodule AdventOfCodeTest.Day6 do
+defmodule AdventOfCodeTest.Day8 do
   use ExUnit.Case
-  doctest AdventOfCode.Day6
+  doctest AdventOfCode.Day8
+
+  [
+    {'""', {2, 0}},
+  ] |> Enum.each(fn {input, {string_literals, memory_values}} ->
+
+    @input input
+    @string_literals string_literals
+    @memory_values memory_values
+
+    test "for input '#{@input}' the string literlas are #{string_literals} and the memory values are #{memory_values}" do
+      {literals, memory} = AdventOfCode.Day8.count_literals_and_memory_for(@input)
+      assert literals == @string_literals
+      assert memory == @memory_values
+    end
+
+  end)
 
 end
