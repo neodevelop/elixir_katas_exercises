d216722a403f5c86c2976cf9058f41fe95ec0f19
neodevelop <juan@makingdevs.com>
2020-04-09 18:10:38 -0500

    Add impl for Day 8 part 2

--- lib/advent_of_code/2017/day8.ex
+++ lib/advent_of_code/2017/day8.ex
@@ -58,4 +58,29 @@ defmodule AdventOfCode.Day8 do
   def count_chars(["\\\\" | t], counter), do: count_chars(t, counter)
   def count_chars([_ | t], counter), do: count_chars(t, counter + 1)
 
+  def differences_encoded_of(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&(String.trim(&1)))
+    |> Enum.map(&(count_encoded_and_literals_for/1))
+    |> compute_differences()
+  end
+
+  def count_encoded_and_literals_for(input) do
+    {count_encoded(input), count_string_literals(input)}
+  end
+
+  def count_encoded(input) do
+    input
+    |> String.split("")
+    |> remove_quotation_marks()
+    |> count_encoded_chars(2) # Because quotation
+  end
+
+  def count_encoded_chars([], counter), do: counter
+  def count_encoded_chars(["\"" | t], counter), do: count_encoded_chars(t, counter + 2)
+  def count_encoded_chars(["\\" | t], counter), do: count_encoded_chars(t, counter + 2)
+  def count_encoded_chars(["\\\\" | t], counter), do: count_encoded_chars(t, counter + 4)
+  def count_encoded_chars([_ | t], counter), do: count_encoded_chars(t, counter + 1)
+
 end
