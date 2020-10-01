d3554c24e094366c47ba4cff2ee772d2b2142c27
Jose Juan Reyes <neodevelop@gmail.com>
2020-09-30 22:54:36 -0500

    Another failed try...

--- lib/advent_of_code/2017/day9.ex
+++ lib/advent_of_code/2017/day9.ex
@@ -1,30 +1,20 @@
 defmodule AdventOfCode.Day9 do
   def trace_routes(vertices) do
     vertices
-    |> uniq_origins()
     |> Enum.map(&trace_routes(&1, vertices))
   end
 
-  defp uniq_origins(vertices) do
-    for({a, b, _} <- vertices, do: [a, b])
-    |> Enum.concat()
-    |> Enum.uniq()
+  defp trace_routes({a, b, d}, vertices) do
+    rest_nodes = Enum.filter(vertices, fn {i, j, _} -> b == i end)
+    next_node = Enum.find(vertices, fn {i, j, _} -> b == i || b == j end)
+    trace_routes(next_node, rest_nodes, {[a], d})
   end
 
-  defp trace_routes(origin, vertices) do
-    next_places = Enum.filter(vertices, fn {a, b, _} -> a == origin || b == origin end)
-    trace_routes(origin, next_places, {[], 0})
-  end
+  defp trace_routes(_, [], route), do: route
 
-  defp trace_routes(_origin, [], route), do: route
-
-  defp trace_routes(origin, [{origin, destiny, distance} | t], {places, total_distance}) do
-    next_places = Enum.filter(t, fn {a, b, _} -> a == destiny || b == destiny end)
-    trace_routes(destiny, next_places, {[origin | places], total_distance + distance})
-  end
-
-  defp trace_routes(origin, [{destiny, origin, distance} | t], {places, total_distance}) do
-    next_places = Enum.filter(t, fn {a, b, _} -> a == destiny || b == destiny end)
-    trace_routes(destiny, next_places, {[origin | places], total_distance + distance})
+  defp trace_routes({a, b, d}, vertices, {places, acc}) do
+    rest_nodes = Enum.filter(vertices, fn {i, j, _} -> b == i end)
+    next_node = Enum.find(vertices, fn {i, j, _} -> b == i || b == j end)
+    trace_routes(next_node, rest_nodes, {places ++ [b], acc + d})
   end
 end
