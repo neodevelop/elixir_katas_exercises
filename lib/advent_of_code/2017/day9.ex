fcf00bee9616c07fb01c880aa74caa5b23dd1807
Jose Juan Reyes <neodevelop@gmail.com>
2020-10-01 16:42:52 -0500

    Another failed try...

--- lib/advent_of_code/2017/day9.ex
+++ lib/advent_of_code/2017/day9.ex
@@ -1,20 +1,52 @@
 defmodule AdventOfCode.Day9 do
   def trace_routes(vertices) do
     vertices
-    |> Enum.map(&trace_routes(&1, vertices))
+    |> uniq_origins
+    |> trace_routes(vertices)
   end
 
-  defp trace_routes({a, b, d}, vertices) do
-    rest_nodes = Enum.filter(vertices, fn {i, j, _} -> b == i end)
-    next_node = Enum.find(vertices, fn {i, j, _} -> b == i || b == j end)
-    trace_routes(next_node, rest_nodes, {[a], d})
+  defp trace_routes(cities, vertices) do
+    vertices
+    |> Enum.map(fn v ->
+      check_cities(Enum.into(cities, %{}, &{&1, false}), v, vertices, cities: [], distance: 0)
+    end)
   end
 
-  defp trace_routes(_, [], route), do: route
+  defp check_cities(_, v, [], info) do
+    IO.inspect(v)
+    info
+  end
 
-  defp trace_routes({a, b, d}, vertices, {places, acc}) do
-    rest_nodes = Enum.filter(vertices, fn {i, j, _} -> b == i end)
-    next_node = Enum.find(vertices, fn {i, j, _} -> b == i || b == j end)
-    trace_routes(next_node, rest_nodes, {places ++ [b], acc + d})
+  defp check_cities(
+         cities,
+         {a, b, d} = v,
+         vertices,
+         [cities: _travel, distance: _distance] = info
+       ) do
+    new_info = trace_route(a, d, info)
+    new_vertices = (vertices -- [v]) |> Enum.filter(fn {i, j, _} -> a != i end)
+    new_cities = Map.replace!(cities, a, true)
+    new_cities = Map.replace!(new_cities, b, true)
+    new_v = Enum.find(new_vertices, fn {i, j, _} -> b == i || b == j end)
+    IO.inspect(new_vertices)
+    IO.inspect(new_cities)
+    IO.inspect(new_info)
+    IO.inspect(new_v)
+    IO.puts("**************************")
+
+    case Enum.all?(new_cities, fn {_, b} -> b end) do
+      true -> trace_route(b, 0, info)
+      false -> check_cities(new_cities, new_v, new_vertices, new_info)
+    end
+  end
+
+  defp trace_route(city, d, cities: cities, distance: distance) do
+    [cities: cities ++ [city], distance: distance + d]
+  end
+
+  defp uniq_origins(vertices) do
+    for({a, b, _} <- vertices, do: [a, b])
+    |> Enum.concat()
+    |> Enum.uniq()
   end
 end
