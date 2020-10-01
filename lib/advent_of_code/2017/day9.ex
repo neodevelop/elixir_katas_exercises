de881aa58677730c3d8d42012a18a522cadd01f2
Jose Juan Reyes <neodevelop@gmail.com>
2020-10-01 17:48:18 -0500

    Add inverted paths

--- lib/advent_of_code/2017/day9.ex
+++ lib/advent_of_code/2017/day9.ex
@@ -1,9 +1,14 @@
 defmodule AdventOfCode.Day9 do
   def trace_routes(vertices) do
     vertices
+    |> add_inverted_paths()
     |> Enum.map(&add_to_routes(&1, vertices))
     |> Enum.map(&sum_distances/1)
     |> min_path()
+  end
+
+  def add_inverted_paths(vertices) do
+    vertices ++ for {a, b, d} <- vertices, do: {b, a, d}
   end
 
   def add_to_routes(v, vertices) do
@@ -14,7 +19,7 @@ defmodule AdventOfCode.Day9 do
   def add_to_routes(_, [], path), do: path
 
   def add_to_routes({a, b, _} = v, vertices, path) do
-    new_vertices = (vertices -- [v]) |> Enum.filter(fn {i, j, _} -> a != i end)
+    new_vertices = (vertices -- [v]) |> Enum.filter(fn {i, j, _} -> a != i && a != j end)
     new_v = Enum.find(new_vertices, fn {i, j, _} -> b == i || b == j end)
     add_to_routes(new_v, new_vertices, path ++ [swap_point(v, new_v)])
   end
