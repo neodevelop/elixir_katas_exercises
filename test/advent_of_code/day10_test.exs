fa11b9e8a229ab2fa04a8e9aed81c5f4ebfe0bcd
Jose Juan Reyes <neodevelop@gmail.com>
2020-10-07 14:30:05 -0500

    Add more tests

--- test/advent_of_code/day10_test.exs
+++ test/advent_of_code/day10_test.exs
@@ -1,9 +1,21 @@
 defmodule AdventOfCodeTest.Day10Test do
   use ExUnit.Case
 
+  test "zero step" do
+    s = "1"
+    result = AdventOfCode.Day10.look_and_say(s)
+    assert "11" == result
+  end
+
   test "first step" do
     s = "1211"
-    result = AdventOfCode.Day10.process(s)
+    result = AdventOfCode.Day10.look_and_say(s)
     assert "111221" == result
+  end
+
+  test "second step" do
+    s = "111221"
+    result = AdventOfCode.Day10.look_and_say(s)
+    assert "312211" == result
   end
 end
