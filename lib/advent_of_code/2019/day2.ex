93f29faf696644eae1ec731ad65223a0d68b3965
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-19 19:19:07 -0600

    Add day2 2019 solution

---
+++ lib/advent_of_code/2019/day2.ex
@@ -0,0 +1,19 @@
+defmodule AdventOfCode.Year2019.Day2 do
+  def execute(program) do
+    program
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> process()
+    |> join()
+  end
+
+  defp process(program_steps) do
+    [2, 0, 0, 0, 99]
+  end
+
+  defp join(new_program) do
+    new_program
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join(",")
+  end
+end
