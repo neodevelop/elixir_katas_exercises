456399bc4a19eeb4776b5ac27b69d99417498a1a
neodevelop <juan@makingdevs.com>
2021-04-04 21:20:06 -0500

    Solve Day3 / 2020

--- lib/advent_of_code/2020/day3.ex
+++ lib/advent_of_code/2020/day3.ex
@@ -1,18 +1,24 @@
 defmodule AdventOfCode.Year2020.Day3 do
 
-  def walk(path, slope \\ 3) do
+  def multiple_walk(path, slopes) do
+    for {horizontal, vertical} <- slopes,
+      do: walk(path, horizontal, vertical)
+  end
+
+  def walk(path, slope \\ 3, vertical_slope \\ 1) do
     path
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(&String.duplicate(&1, 100))
     |> List.pop_at(0)
     |> elem(1)
-    |> find_trees([], 1, slope)
+    |> find_trees([], 1, slope, vertical_slope)
     |> count_trees()
   end
 
-  defp find_trees([], walked, _, _), do: walked
-  defp find_trees([path | rest], walked, positions, slope) do
+  defp find_trees([], walked, _, _, _), do: walked
+  defp find_trees(forest, walked, positions, slope, vertical_slope) do
+    [path | rest] = forest |> walk_vertical_slope(vertical_slope - 1)
     new_path = path |> String.split("")
 
     new_path = case new_path |> Enum.fetch!(positions + slope) do
@@ -20,7 +26,7 @@ defmodule AdventOfCode.Year2020.Day3 do
       "#" -> new_path |> List.replace_at(positions + slope, "X")
     end
 
-    find_trees(rest, walked ++ [new_path], positions + slope, slope)
+    find_trees(rest, walked ++ [new_path], positions + slope, slope, vertical_slope)
   end
 
   defp count_trees(path) do
@@ -28,6 +34,11 @@ defmodule AdventOfCode.Year2020.Day3 do
     |> Enum.map(fn l -> Enum.filter(l, fn e -> e == "X" end) end)
     |> Enum.flat_map(fn e -> e end)
     |> Enum.count()
+  end
+
+  defp walk_vertical_slope(forest, 0), do: forest
+  defp walk_vertical_slope([_path | rest], vertical_slope) do
+    walk_vertical_slope(rest, vertical_slope - 1)
   end
 
 end
--- test/advent_of_code/2020/day3_test.exs
+++ test/advent_of_code/2020/day3_test.exs
@@ -1,25 +1,36 @@
 defmodule AdventOfCode.Year2020.Day3Test do
   use ExUnit.Case
 
-  test "find the trees in the path" do
-    path = """
-		..##.......
-		#...#...#..
-		.#....#..#.
-		..#.#...#.#
-		.#...##..#.
-		..#.##.....
-		.#.#.#....#
-		.#........#
-		#.##...#...
-		#...##....#
-		.#..#...#.#
-    """
+  setup do
+    %{
+      path:
+      """
+      ..##.......
+      #...#...#..
+      .#....#..#.
+      ..#.#...#.#
+      .#...##..#.
+      ..#.##.....
+      .#.#.#....#
+      .#........#
+      #.##...#...
+      #...##....#
+      .#..#...#.#
+      """
+    }
+  end
 
+  test "find the trees in the path", %{path: path} do
     trees = AdventOfCode.Year2020.Day3.walk(path)
     assert trees == 7
   end
 
+  test "find the trees in multiple paths", %{path: path} do
+    slopes = [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+
+    trees = AdventOfCode.Year2020.Day3.multiple_walk(path, slopes)
+    assert trees == [2, 7, 3, 4, 2]
+  end
 
 end
 
