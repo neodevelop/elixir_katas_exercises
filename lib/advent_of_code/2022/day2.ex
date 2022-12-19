c0fbfc9ff375da9b853ed9d17123d71470e542ef
Jose Juan Reyes <neodevelop@gmail.com>
2022-12-18 22:43:36 -0600

    Solves Part 2 Day 2 - 2022

--- lib/advent_of_code/2022/day2.ex
+++ lib/advent_of_code/2022/day2.ex
@@ -1,17 +1,30 @@
 defmodule AdventOfCode.Year2022.Day2 do
 
-
 	def compute_score(guide) do
     guide
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(&input_player_1/1)
+    |> handle_input()
     |> Enum.map(&input_player_2/1)
     |> Enum.map(&play_game/1)
     |> Enum.map(&get_points/1)
     |> Enum.sum()
 	end
+
+  def compute_score_2(guide) do
+    guide
+    |> handle_input()
+    |> Enum.map(&expected_result/1)
+    |> Enum.map(&play_game_2/1)
+    |> Enum.map(&get_points/1)
+    |> Enum.sum()
+  end
+
+  defp handle_input(guide) do
+    guide
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(&input_player_1/1)
+  end
 
   defp input_player_1(["A", e]), do: [:rock, e]
   defp input_player_1(["B", e]), do: [:paper, e]
@@ -21,6 +34,10 @@ defmodule AdventOfCode.Year2022.Day2 do
   defp input_player_2([e, "Y"]), do: [e, :paper]
   defp input_player_2([e, "Z"]), do: [e, :scissors]
 
+  defp expected_result([e, "X"]), do: [e, :lost]
+  defp expected_result([e, "Y"]), do: [e, :draw]
+  defp expected_result([e, "Z"]), do: [e, :won]
+
   defp play_game([:rock, :scissors]), do: [:rock, :scissors, :lost]
   defp play_game([:rock, :paper]), do: [:rock, :paper, :won]
   defp play_game([:paper, :rock]), do: [:paper, :rock, :lost]
@@ -28,6 +45,14 @@ defmodule AdventOfCode.Year2022.Day2 do
   defp play_game([:scissors, :paper]), do: [:scissors, :paper, :lost]
   defp play_game([:scissors, :rock]), do: [:scissors, :rock, :won]
   defp play_game([a, a]), do: [a, a, :draw]
+
+  defp play_game_2([:rock, :lost]), do: [:rock, :scissors, :lost]
+  defp play_game_2([:rock, :won]), do: [:rock, :paper, :won]
+  defp play_game_2([:paper, :lost]), do: [:paper, :rock, :lost]
+  defp play_game_2([:paper, :won]), do: [:paper, :scissors, :won]
+  defp play_game_2([:scissors, :lost]), do: [:scissors, :paper, :lost]
+  defp play_game_2([:scissors, :won]), do: [:scissors, :rock, :won]
+  defp play_game_2([a, :draw]), do: [a, a, :draw]
 
   defp get_points([_, :rock, status]), do: 1 + get_status_points(status)
   defp get_points([_, :paper, status]), do: 2 + get_status_points(status)
--- test/advent_of_code/2022/day2_test.exs
+++ test/advent_of_code/2022/day2_test.exs
@@ -15,5 +15,9 @@ defmodule AdventOfCode.Year2020.Day3Test do
     assert 15 == AdventOfCode.Year2022.Day2.compute_score(guide)
   end
 
+  test "get total score for the guide figure out the round", %{guide: guide} do
+    assert 12 == AdventOfCode.Year2022.Day2.compute_score_2(guide)
+  end
+
 end
 
