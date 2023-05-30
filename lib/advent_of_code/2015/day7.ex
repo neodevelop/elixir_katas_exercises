d037202eb0d37ab64ac50a6653652ccbd836e1c7
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-05-29 22:39:00 -0600

    Try for dependant circuits

--- lib/advent_of_code/2015/day7.ex
+++ lib/advent_of_code/2015/day7.ex
@@ -5,17 +5,17 @@ defmodule AdventOfCode.Day7 do
     signals =
       circuit
       |> circuit_in_operators()
-      |> Enum.filter(&get_single_values/1)
+      |> apply_operations(%{})
 
-    initial_values =
-      for [value, variable] <- signals,
-          into: %{},
-          do: {variable, value}
+    # initial_values =
+    #   for [value, variable] <- signals,
+    #       into: %{},
+    #       do: {variable, value}
 
-    circuit
-    |> circuit_in_operators()
-    |> Enum.filter(&get_operations/1)
-    |> apply_operations(initial_values)
+    # circuit
+    # |> circuit_in_operators()
+    # |> Enum.filter(&get_operations/1)
+    # |> apply_operations(initial_values)
   end
 
   defp circuit_in_operators(circuit) do
@@ -23,7 +23,6 @@ defmodule AdventOfCode.Day7 do
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(&split_operations/1)
-    |> Enum.map(&transform_single_input/1)
     |> Enum.map(&transform_operations/1)
   end
 
@@ -33,65 +32,61 @@ defmodule AdventOfCode.Day7 do
     |> Enum.map(&String.trim/1)
   end
 
-  defp transform_single_input([operation, variable] = input) do
-    Regex.match?(~r/^\d+$/, operation)
-    |> case do
-      true -> [String.to_integer(operation), variable]
-      false -> input
-    end
-  end
-
-  defp transform_operations([operation, _] = input) when is_integer(operation), do: input
-
   defp transform_operations([operation, variable]) do
     [String.split(operation, " "), variable]
   end
 
-  defp get_single_values([v, _]) when is_integer(v), do: true
-  defp get_single_values(_), do: false
-
-  defp get_operations([v, _]) when is_integer(v), do: false
-  defp get_operations(_), do: true
-
   defp apply_operations([], values), do: values
 
-  defp apply_operations([[operation, var] | rest], values) do
+  defp apply_operations([[operation, var] | rest] = operations, values) do
     result = apply_single_operation(operation, values)
+
     apply_operations(rest, Map.put(values, var, result))
   end
 
-  defp apply_single_operation(["NOT", v], values) do
-    values
-    |> Map.get(v)
-    |> Bitwise.bnot()
-    |> Kernel.+(@limit)
+  defp apply_single_operation([a, "AND", b], _values) when is_integer(a) and is_integer(b) do
+    Bitwise.band(a, b)
   end
 
-  defp apply_single_operation([a, "AND", b], values) do
-    value_a = Map.get(values, a)
-    value_b = Map.get(values, b)
-    Bitwise.band(value_a, value_b)
+  defp apply_single_operation([a, "AND", b], _values) do
   end
 
-  defp apply_single_operation([a, "OR", b], values) do
-    value_a = Map.get(values, a)
-    value_b = Map.get(values, b)
-    Bitwise.bor(value_a, value_b)
+  defp apply_single_operation([a, "AND", b], _values) do
+    apply_single_operation
   end
 
-  defp apply_single_operation([a, "LSHIFT", b], values) do
-    value_a = Map.get(values, a)
-    value_b = String.to_integer(b)
-    Bitwise.bsl(value_a, value_b)
-  end
+  # defp apply_single_operation(["NOT", v], values) do
+  #   values
+  #   |> Map.get(v)
+  #   |> Bitwise.bnot()
+  #   |> Kernel.+(@limit)
+  # end
 
-  defp apply_single_operation([a, "RSHIFT", b], values) do
-    value_a = Map.get(values, a)
-    value_b = String.to_integer(b)
-    Bitwise.bsr(value_a, value_b)
-  end
+  # defp apply_single_operation([a, "AND", b], values) do
+  #   value_a = Map.get(values, a)
+  #   value_b = Map.get(values, b)
+  #   Bitwise.band(value_a, value_b)
+  # end
 
-  defp apply_single_operation(operation, _values) do
-    operation
-  end
+  # defp apply_single_operation([a, "OR", b], values) do
+  #   value_a = Map.get(values, a)
+  #   value_b = Map.get(values, b)
+  #   Bitwise.bor(value_a, value_b)
+  # end
+
+  # defp apply_single_operation([a, "LSHIFT", b], values) do
+  #   value_a = Map.get(values, a)
+  #   value_b = String.to_integer(b)
+  #   Bitwise.bsl(value_a, value_b)
+  # end
+
+  # defp apply_single_operation([a, "RSHIFT", b], values) do
+  #   value_a = Map.get(values, a)
+  #   value_b = String.to_integer(b)
+  #   Bitwise.bsr(value_a, value_b)
+  # end
+
+  # defp apply_single_operation(operation, _values) do
+  #   operation
+  # end
 end
