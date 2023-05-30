192536a6b191f29eb43a7c685ad21c393e09e050
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-05-29 22:34:40 -0600

    Add a test with dependant circuit

--- test/advent_of_code/2015/day7_test.exs
+++ test/advent_of_code/2015/day7_test.exs
@@ -31,5 +31,37 @@ defmodule AdventOfCodeTest.Day7Test do
 
       assert expected_signals == signals_on_wires
     end
+
+    test "run the circuit with anon vars" do
+      circuit = """
+      d AND e -> a
+      a -> z
+      123 -> x
+      456 -> y
+      x AND y -> d
+      x OR y -> e
+      x LSHIFT 2 -> f
+      y RSHIFT 2 -> g
+      NOT x -> h
+      NOT y -> i
+      """
+
+      signals_on_wires = Day7.emulate_the_circuit(circuit)
+
+      expected_signals = %{
+        "d" => 72,
+        "e" => 507,
+        "f" => 492,
+        "g" => 114,
+        "h" => 65412,
+        "i" => 65079,
+        "x" => 123,
+        "y" => 456,
+        "a" => 72,
+        "z" => 72
+      }
+
+      assert expected_signals == signals_on_wires
+    end
   end
 end
