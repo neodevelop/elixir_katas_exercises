54453d677a9de9230f4da3fe1592f73c8d57eafe
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-06-11 15:52:50 -0600

    Solves Day 7 2015, double case :S for validations!!!

--- lib/advent_of_code/2015/day7.ex
+++ lib/advent_of_code/2015/day7.ex
@@ -40,170 +40,130 @@ defmodule AdventOfCode.Day7 do
     [new_operators, variable]
   end
 
-  defp apply_operations(operations) do
-    apply_operations(operations, operations, %{})
+  defp build_circuit(operations) do
+    build_circuit(operations, operations, %{})
   end
 
-  defp apply_operations([], _operations, values), do: values
-
-  defp apply_operations([operation | rest], operations, values) do
-    apply_single_operation(operation, operations, values)
-
-    apply_operations(rest, operations, values)
-  end
-
-  defp apply_single_operation([[operator], var], _operations, values)
-       when is_integer(operator) do
-    values
-    |> Map.put(var, operator)
-    |> IO.inspect(label: "Single value")
-  end
-
-  defp apply_single_operation([[operation], variable], operations, values) do
-    IO.inspect(binding(), label: "single value")
-
-    case String.match?(operation, @is_number) do
-      true ->
-        [[String.to_integer(operation)], variable]
-        |> apply_single_operation(operations, values)
-
-      false ->
-        operations
-        |> Enum.find(fn [_, v] -> operation == v end)
-        |> IO.inspect()
-        |> apply_single_operation(operations, values)
+  defp build_circuit([], operations, circuit) do
+    case Enum.count(operations) == Enum.count(circuit) do
+      true -> circuit
+      false -> build_circuit(operations, operations, circuit)
     end
   end
 
-  defp apply_single_operation([[operator, op], var], _operations, values)
-       when is_integer(op) do
-    values
-    |> Map.put(var, exec_operation(operator, op))
+  defp build_circuit([operation | rest], operations, circuit) do
+    new_circuit = wire(operation, operations, circuit)
+
+    build_circuit(rest, operations, new_circuit)
   end
 
-  defp apply_single_operation([[operator, op], variable], operations, values) do
-    case String.match?(op, @is_number) do
-      true ->
-        [[operator, String.to_integer(op)], variable]
-        |> apply_single_operation(operations, values)
-
-      false ->
-        operations
-        |> Enum.find(fn [_, v] -> op == v end)
-        |> IO.inspect()
-        |> apply_single_operation(operations, values)
-    end
-  end
-
-  defp apply_single_operation([[op_1, operator, op_2], variable], operations, values)
+  defp wire([[op_1, operator, op_2], variable], operations, circuit)
        when is_binary(op_1) do
-    case String.match?(op_1, @is_number) do
+    case Map.has_key?(circuit, op_1) do
       true ->
-        [[String.to_integer(op_1), operator, op_2], variable]
-        |> apply_single_operation(operations, values)
+        [[Map.get(circuit, op_1), operator, op_2], variable]
+        |> wire(operations, circuit)
 
       false ->
-        operations
-        |> Enum.find(fn [_, v] -> op_1 == v end)
-        |> IO.inspect()
-        |> apply_single_operation(operations, values)
+        case String.match?(op_1, @is_number) do
+          true ->
+            [[String.to_integer(op_1), operator, op_2], variable]
+            |> wire(operations, circuit)
+
+          false ->
+            operations
+            |> Enum.find(fn [_, v] -> op_1 == v end)
+            |> wire(operations, circuit)
+        end
     end
   end
 
-  defp apply_single_operation([[op_1, operator, op_2], variable], operations, values)
+  defp wire([[op_1, operator, op_2], variable], operations, circuit)
        when is_binary(op_2) do
-    case String.match?(op_2, @is_number) do
+    case Map.has_key?(circuit, op_2) do
       true ->
-        [[op_1, operator, String.to_integer(op_2)], variable]
-        |> apply_single_operation(operations, values)
+        [[op_1, operator, Map.get(circuit, op_2)], variable]
+        |> wire(operations, circuit)
 
       false ->
-        operations
-        |> Enum.find(fn [_, v] -> op_2 == v end)
-        |> IO.inspect()
-        |> apply_single_operation(operations, values)
+        case String.match?(op_2, @is_number) do
+          true ->
+            [[op_1, operator, String.to_integer(op_2)], variable]
+            |> wire(operations, circuit)
+
+          false ->
+            operations
+            |> Enum.find(fn [_, v] -> op_2 == v end)
+            |> wire(operations, circuit)
+        end
     end
   end
 
-  defp apply_single_operation([[op_1, operator, op_2], var], _operations, values)
+  defp wire([[operator, op], variable], operations, circuit) when is_binary(op) do
+    case Map.has_key?(circuit, op) do
+      true ->
+        [[operator, Map.get(circuit, op)], variable]
+        |> wire(operations, circuit)
+
+      false ->
+        case String.match?(op, @is_number) do
+          true ->
+            [[operator, String.to_integer(op)], variable]
+            |> wire(operations, circuit)
+
+          false ->
+            operations
+            |> Enum.find(fn [_, v] -> op == v end)
+            |> wire(operations, circuit)
+        end
+    end
+  end
+
+  defp wire([[operation], variable], operations, circuit) when is_binary(operation) do
+    case Map.has_key?(circuit, operation) do
+      true ->
+        circuit
+        |> Map.put(variable, Map.get(circuit, operation))
+
+      false ->
+        case String.match?(operation, @is_number) do
+          true ->
+            circuit
+            |> Map.put(variable, Map.get(circuit, operation))
+
+          false ->
+            operations
+            |> Enum.find(fn [_, v] -> operation == v end)
+            |> wire(operations, circuit)
+        end
+    end
+  end
+
+  defp wire([[op_1, operator, op_2], variable], _operations, circuit)
        when is_integer(op_1) and is_integer(op_2) do
-    values
-    |> Map.put(var, exec_operation(op_1, operator, op_2))
+    circuit
+    |> Map.put(variable, exec_operation(op_1, operator, op_2))
   end
 
-  defp apply_single_operation([_, variable], operations, values) do
-    operations
-    |> Enum.find(fn [_, v] -> variable == v end)
-    |> apply_single_operation(operations, values)
+  defp wire([[operator, op], variable], _operations, circuit)
+       when is_integer(op) do
+    circuit
+    |> Map.put(variable, exec_operation(operator, op))
   end
 
-  # defp apply_operations([], operations, values) do
-  #   case Enum.count(operations) == Enum.count(values) do
-  #     true -> values
-  #     false -> apply_operations(operations, operations, values)
-  #   end
-  # end
+  defp wire([[operator], variable], _operations, circuit) when is_integer(operator) do
+    circuit
+    |> Map.put(variable, operator)
+  end
 
-  # defp apply_operations([[operation, var] | rest], operations, values) do
-  #   result = apply_single_operation(operation, values)
-
-  #   case is_integer(result) do
-  #     true ->
-  #       apply_operations(rest, operations, Map.put(values, var, result))
-
-  #     false ->
-  #       apply_operations(rest, operations, values)
-  #   end
-  # end
-
-  # defp apply_single_operation([v], values) do
-  #   case Regex.match?(~r/^\d+$/, v) do
-  #     true ->
-  #       String.to_integer(v)
-
-  #     false ->
-  #       case Map.get(values, v) do
-  #         nil -> v
-  #         e -> e
-  #       end
-  #   end
-  # end
-
-  # defp apply_single_operation([operand, a] = operation, values) do
-  #   case Map.has_key?(values, a) do
-  #     true -> exec_operation(operand, a, values)
-  #     false -> operation
-  #   end
-  # end
-
-  # defp apply_single_operation([a, operand, b] = operation, values)
-  #      when operand in ["AND", "OR"] do
-  #   case Map.has_key?(values, a) and Map.has_key?(values, b) do
-  #     true -> exec_operation(a, operand, b, values)
-  #     false -> operation
-  #   end
-  # end
-
-  # defp apply_single_operation([a, operand, b] = operation, values)
-  #      when operand in ["LSHIFT", "RSHIFT"] do
-  #   case Map.has_key?(values, a) do
-  #     true -> exec_operation(a, operand, b, values)
-  #     false -> operation
-  #   end
-  # end
-
-  # defp apply_single_operation(operation, _values) do
-  #   operation
-  # end
+  defp exec_operation(a, "AND", b), do: Bitwise.band(a, b)
+  defp exec_operation(a, "OR", b), do: Bitwise.bor(a, b)
+  defp exec_operation(a, "LSHIFT", b), do: Bitwise.bsl(a, b)
+  defp exec_operation(a, "RSHIFT", b), do: Bitwise.bsr(a, b)
 
   defp exec_operation("NOT", v),
     do:
       v
       |> Bitwise.bnot()
       |> Kernel.+(@limit)
-
-  defp exec_operation(a, "AND", b), do: Bitwise.band(a, b)
-  defp exec_operation(a, "OR", b), do: Bitwise.bor(a, b)
-  defp exec_operation(a, "LSHIFT", b), do: Bitwise.bsl(a, b)
-  defp exec_operation(a, "RSHIFT", b), do: Bitwise.bsr(a, b)
 end
