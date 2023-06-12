bf1c687bb8aa32322115c65f2e7d1b3100f6b4e9
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-06-11 22:51:18 -0600

    Solves Day 1 - 2018

--- lib/advent_of_code/2018/day1.ex
+++ lib/advent_of_code/2018/day1.ex
@@ -1,32 +1,41 @@
 defmodule AdventOfCode.Year2018.Day1 do
-
-  def sum_numbers([], accum, _sums) do
-    accum
+  def sum_numbers(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 
-  def sum_numbers([h|t], accum, sums) do
-
-    new_frequency = accum + h
-    counter = case Map.get(sums, new_frequency) do
-      nil -> 0
-      x -> x + 1
-    end
-
-    new_sums = sums |> Map.put(new_frequency, counter)
-
-    # IO.puts """
-    #   New frequency: #{new_frequency}
-    #   Current sum: #{accum}
-    #   Sums: #{inspect sums}
-    # """
-
-    case Enum.find(sums, fn {_, n} -> n > 0 end) do
-      {x, _} -> IO.puts "Twice #{x}"
-      nil -> :noop
-    end
-
-    sum_numbers(t, new_frequency, new_sums)
+  def find_frequency_twice(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> find_frequency()
   end
 
+  defp find_frequency(l) do
+    find_and_cycle(l, l, [0])
+  end
+
+  defp find_and_cycle([], original_list, freqs) do
+    find_and_cycle(original_list, original_list, freqs)
+  end
+
+  defp find_and_cycle([h | t], original_list, freqs) do
+    last =
+      List.last(freqs)
+      |> Kernel.+(h)
+
+    freqs
+    |> Enum.any?(fn e -> e == last end)
+    |> case do
+      true ->
+        last
+
+      false ->
+        find_and_cycle(t, original_list, freqs ++ [last])
+    end
+  end
 end
-
