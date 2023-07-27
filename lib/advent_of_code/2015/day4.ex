1dae7560cfba1ae167438aa05bd66be0350c81f6
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-07-26 23:06:48 -0600

    Integer.to_char_list is deprecated...

--- lib/advent_of_code/2015/day4.ex
+++ lib/advent_of_code/2015/day4.ex
@@ -1,16 +1,28 @@
 defmodule AdventOfCode.Day4 do
-
   def assemble(key, n) do
-    s = cond do
-      n >= 0 && n < 10 -> '00000' ++ Integer.to_char_list n
-      n >= 10 && n < 100 -> '0000' ++ Integer.to_char_list n
-      n >= 100 && n < 1000 -> '000' ++ Integer.to_char_list n
-      n >= 1000 && n < 10000 -> '00' ++ Integer.to_char_list n
-      n >= 10000 && n < 100000 -> '0' ++ Integer.to_char_list n
-      true -> Integer.to_char_list n
-      # n >= 100000 && n < 1000000 -> Integer.to_char_list n
-      # n >= 1000000 -> Integer.to_char_list n
-    end
+    s =
+      cond do
+        n >= 0 && n < 10 ->
+          '00000' ++ Integer.to_charlist(n)
+
+        n >= 10 && n < 100 ->
+          '0000' ++ Integer.to_charlist(n)
+
+        n >= 100 && n < 1000 ->
+          '000' ++ Integer.to_charlist(n)
+
+        n >= 1000 && n < 10000 ->
+          '00' ++ Integer.to_charlist(n)
+
+        n >= 10000 && n < 100_000 ->
+          '0' ++ Integer.to_charlist(n)
+
+        true ->
+          Integer.to_charlist(n)
+          # n >= 100000 && n < 1000000 -> Integer.to_charlist n
+          # n >= 1000000 -> Integer.to_char_list n
+      end
+
     key ++ s
   end
 
@@ -19,32 +31,31 @@ defmodule AdventOfCode.Day4 do
   end
 
   def searchForSecret(_secret, n, '000000') do
-    n-1
+    n - 1
   end
 
   def searchForSecret(secret, n, _) do
     l = makeTheHash(secret, n)
-    searchForSecret(secret, n+1, Enum.take(l,5))
+    searchForSecret(secret, n + 1, Enum.take(l, 5))
   end
 
   def makeTheHash(secret, n) do
-    :crypto.hash(:md5 , assemble(secret,n))
+    :crypto.hash(:md5, assemble(secret, n))
     |> Base.encode16()
-    |> String.to_char_list
+    |> String.to_charlist()
   end
 
   def first_part(secret) do
-    1..1000000
+    1..1_000_000
     |> Stream.map(fn x -> {x, AdventOfCode.Day4.makeTheHash(secret, x)} end)
     |> Stream.filter(fn {_, h} -> Enum.take(h, 5) == '00000' end)
-    |> Enum.to_list
+    |> Enum.to_list()
   end
 
   def second_part(secret) do
-    1..10000000
+    1..10_000_000
     |> Stream.map(fn x -> {x, AdventOfCode.Day4.makeTheHash(secret, x)} end)
     |> Stream.filter(fn {_, h} -> Enum.take(h, 6) == '000000' end)
-    |> Enum.to_list
+    |> Enum.to_list()
   end
-
 end
