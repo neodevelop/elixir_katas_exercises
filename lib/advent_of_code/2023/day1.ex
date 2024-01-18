7fa2842791f080ef3e2f7f440d166e4cd484e0e9
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-01-17 18:27:23 -0600

    Solves Day 1 Year 2023

---
+++ lib/advent_of_code/2023/day1.ex
@@ -0,0 +1,23 @@
+defmodule AdventOfCode.Year2023.Day1 do
+  @numbers ~s/one two three four five six seven eight nine/
+
+  def calibrate(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&found_calibration/1)
+    |> Enum.sum()
+  end
+
+  defp found_calibration(value) do
+    ~r/\d/
+    |> Regex.scan(value)
+    |> Enum.map(fn [e] -> e end)
+    |> join_first_and_last()
+  end
+
+  defp join_first_and_last(matches) do
+    [List.first(matches), List.last(matches)]
+    |> Enum.join()
+    |> String.to_integer()
+  end
+end
