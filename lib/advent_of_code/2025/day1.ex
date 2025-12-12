7414d95d83924f9ed6922b91005d628093fd35f1
José Juan Reyes Zuñiga <juan@makingdevs.com>
2025-12-11 22:13:40 -0600

    Advance fail: Day 1 - Part 2 - 2025

--- lib/advent_of_code/2025/day1.ex
+++ lib/advent_of_code/2025/day1.ex
@@ -12,17 +12,26 @@ defmodule AdventOfCode.Year2025.Day1 do
   end
 
   def password_method(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn
-      <<?R::utf8, n::binary>> -> {:right, String.to_integer(n)}
-      <<?L::utf8, n::binary>> -> {:left, String.to_integer(n)}
-    end)
-    |> Enum.map_reduce(50, &rotate_to_once(&1, &2))
-    |> elem(0)
-    |> Enum.map(fn {_, _, {counter, _}} -> counter end)
-    |> Enum.count(&(&1 > 0))
-    |> dbg()
+    spins =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn
+        <<?R::utf8, n::binary>> -> {:right, String.to_integer(n)}
+        <<?L::utf8, n::binary>> -> {:left, String.to_integer(n)}
+      end)
+      |> Enum.map_reduce(50, &rotate_to_once(&1, &2))
+      |> elem(0)
+
+    zero_dial = spins |> Enum.count(fn {_, _, {_, dial}} -> dial == 0 end)
+
+    zero_once_dial =
+      spins
+      |> Enum.filter(fn {_, _, {counter, _dial}} -> counter > 0 end)
+      |> Enum.map(fn {_, _, {counter, _}} -> counter end)
+      |> Enum.sum()
+      |> dbg()
+
+    zero_dial + zero_once_dial
   end
 
   defp rotate({:right, n}, dial) do
@@ -44,19 +53,33 @@ defmodule AdventOfCode.Year2025.Day1 do
   defp decrement(n, dial), do: decrement(n - 1, dial - 1)
 
   defp rotate_to_once({:right, n}, dial) do
-    {counter, new_dial} = increment_to_once(n, 0, dial)
+    {counter, new_dial} =
+      case dial do
+        99 -> increment_to_once(n, -1, 99)
+        _ -> increment_to_once(n, 0, dial)
+      end
+
     {{:right, n, {counter, new_dial}}, new_dial}
   end
 
   defp rotate_to_once({:left, n}, dial) do
-    {counter, new_dial} = decrement_to_once(n, 0, dial)
+    {counter, new_dial} =
+      case dial do
+        0 -> decrement_to_once(n, -1, 0)
+        _ -> decrement_to_once(n, 0, dial)
+      end
+
     {{:left, n, {counter, new_dial}}, new_dial}
   end
 
+  defp increment_to_once(0, 1, 0), do: {0, 0}
+  defp increment_to_once(0, counter, 0), do: {counter, 0}
   defp increment_to_once(0, counter, dial), do: {counter, dial}
   defp increment_to_once(n, counter, 99), do: increment_to_once(n - 1, counter + 1, 0)
   defp increment_to_once(n, counter, dial), do: increment_to_once(n - 1, counter, dial + 1)
 
+  defp decrement_to_once(0, 1, 0), do: {0, 0}
+  defp decrement_to_once(0, counter, 0), do: {counter, 0}
   defp decrement_to_once(0, counter, dial), do: {counter, dial}
   defp decrement_to_once(n, counter, 0), do: decrement_to_once(n - 1, counter + 1, 99)
   defp decrement_to_once(n, counter, dial), do: decrement_to_once(n - 1, counter, dial - 1)
