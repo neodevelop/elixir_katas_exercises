a8c244cbd8fdeb76a58258fd4faf31fc0600a9ed
José Juan Reyes Zuñiga <juan@makingdevs.com>
2025-12-11 20:23:51 -0600

    Advance: Day 1 - Part 2 - 2025

--- lib/advent_of_code/2025/day1.ex
+++ lib/advent_of_code/2025/day1.ex
@@ -11,6 +11,20 @@ defmodule AdventOfCode.Year2025.Day1 do
     |> Enum.count(fn {_, _, dial} -> dial == 0 end)
   end
 
+  def password_method(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn
+      <<?R::utf8, n::binary>> -> {:right, String.to_integer(n)}
+      <<?L::utf8, n::binary>> -> {:left, String.to_integer(n)}
+    end)
+    |> Enum.map_reduce(50, &rotate_to_once(&1, &2))
+    |> elem(0)
+    |> Enum.map(fn {_, _, {counter, _}} -> counter end)
+    |> Enum.count(&(&1 > 0))
+    |> dbg()
+  end
+
   defp rotate({:right, n}, dial) do
     new_dial = increment(n, dial)
     {{:right, n, new_dial}, new_dial}
@@ -28,4 +42,22 @@ defmodule AdventOfCode.Year2025.Day1 do
   defp decrement(0, dial), do: dial
   defp decrement(n, 0), do: decrement(n - 1, 99)
   defp decrement(n, dial), do: decrement(n - 1, dial - 1)
+
+  defp rotate_to_once({:right, n}, dial) do
+    {counter, new_dial} = increment_to_once(n, 0, dial)
+    {{:right, n, {counter, new_dial}}, new_dial}
+  end
+
+  defp rotate_to_once({:left, n}, dial) do
+    {counter, new_dial} = decrement_to_once(n, 0, dial)
+    {{:left, n, {counter, new_dial}}, new_dial}
+  end
+
+  defp increment_to_once(0, counter, dial), do: {counter, dial}
+  defp increment_to_once(n, counter, 99), do: increment_to_once(n - 1, counter + 1, 0)
+  defp increment_to_once(n, counter, dial), do: increment_to_once(n - 1, counter, dial + 1)
+
+  defp decrement_to_once(0, counter, dial), do: {counter, dial}
+  defp decrement_to_once(n, counter, 0), do: decrement_to_once(n - 1, counter + 1, 99)
+  defp decrement_to_once(n, counter, dial), do: decrement_to_once(n - 1, counter, dial - 1)
 end
