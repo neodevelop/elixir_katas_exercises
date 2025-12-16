8eb768663fdb02d97039884074fbb78ddd25c8dd
José Reyes Zuñiga <juan@makingdevs.com>
2025-12-15 22:00:46 -0600

    Day 1 - 2025 - Part 2

--- lib/advent_of_code/2025/day1.ex
+++ lib/advent_of_code/2025/day1.ex
@@ -12,26 +12,17 @@ defmodule AdventOfCode.Year2025.Day1 do
   end
 
   def password_method(input) do
-    spins =
-      input
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn
-        <<?R::utf8, n::binary>> -> {:right, String.to_integer(n)}
-        <<?L::utf8, n::binary>> -> {:left, String.to_integer(n)}
-      end)
-      |> Enum.map_reduce(50, &rotate_to_once(&1, &2))
-      |> elem(0)
-
-    zero_dial = spins |> Enum.count(fn {_, _, {_, dial}} -> dial == 0 end)
-
-    zero_once_dial =
-      spins
-      |> Enum.filter(fn {_, _, {counter, _dial}} -> counter > 0 end)
-      |> Enum.map(fn {_, _, {counter, _}} -> counter end)
-      |> Enum.sum()
-      |> dbg()
-
-    zero_dial + zero_once_dial
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn
+      <<?R::utf8, n::binary>> -> {:right, String.to_integer(n)}
+      <<?L::utf8, n::binary>> -> {:left, String.to_integer(n)}
+    end)
+    |> Enum.reduce({50, 0}, fn move, {dial, acc} ->
+      {zeros, new_dial} = rotate_count(move, dial)
+      {new_dial, acc + zeros}
+    end)
+    |> elem(1)
   end
 
   defp rotate({:right, n}, dial) do
@@ -52,40 +43,30 @@ defmodule AdventOfCode.Year2025.Day1 do
   defp decrement(n, 0), do: decrement(n - 1, 99)
   defp decrement(n, dial), do: decrement(n - 1, dial - 1)
 
-  defp rotate_to_once({:right, n}, dial) do
-    new_dial = rem(dial + n, 100)
-    counter = div(dial + n, 100)
-
-    {{:right, n, {counter, new_dial}}, new_dial}
+  defp rotate_count({:right, n}, s) do
+    zeros = count_hits_right(s, n)
+    new_dial = rem(s + n, 100)
+    {zeros, new_dial}
   end
 
-  defp rotate_to_once({:left, n}, dial) do
-    new_dial =
-      rem(dial - n, 100)
-      |> case do
-        n when n < 0 -> (n + 100) |> abs()
-        n -> n
-      end
-
-    counter =
-      (dial - n)
-      |> case do
-        n when n < 0 -> div(dial - n, 100) + if(new_dial == 0, do: 0, else: 1)
-        _ -> 0
-      end
-
-    {{:left, n, {counter, new_dial}}, new_dial}
+  defp rotate_count({:left, n}, s) do
+    zeros = count_hits_left(s, n)
+    new_dial = rem(s - n, 100)
+    new_dial = if new_dial < 0, do: new_dial + 100, else: new_dial
+    {zeros, new_dial}
   end
 
-  # defp increment_to_once(0, 1, 0), do: {0, 0}
-  # defp increment_to_once(0, counter, 0), do: {counter, 0}
-  # defp increment_to_once(0, counter, dial), do: {counter, dial}
-  # defp increment_to_once(n, counter, 99), do: increment_to_once(n - 1, counter + 1, 0)
-  # defp increment_to_once(n, counter, dial), do: increment_to_once(n - 1, counter, dial + 1)
+  defp count_hits_right(0, n), do: div(n, 100)
 
-  # defp decrement_to_once(0, 1, 0), do: {0, 0}
-  # defp decrement_to_once(0, counter, 0), do: {counter, 0}
-  # defp decrement_to_once(0, counter, dial), do: {counter, dial}
-  # defp decrement_to_once(n, counter, 0), do: decrement_to_once(n - 1, counter + 1, 99)
-  # defp decrement_to_once(n, counter, dial), do: decrement_to_once(n - 1, counter, dial - 1)
+  defp count_hits_right(s, n) do
+    first = 100 - s
+    if n < first, do: 0, else: 1 + div(n - first, 100)
+  end
+
+  defp count_hits_left(0, n), do: div(n, 100)
+
+  defp count_hits_left(s, n) do
+    first = s
+    if n < first, do: 0, else: 1 + div(n - first, 100)
+  end
 end
